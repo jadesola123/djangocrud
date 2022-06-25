6711957e299830c0b580f89f9404bad4cdeafb2f
Your Namejadesola123 <jadesolak.business@gmail.com>
2022-06-25 13:51:11 +0100

    commmit

---
+++ I4G000226MRB/blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+# Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    queryset = Post.objects.all().order_by("-date_created")
+
+class PostCreateView(CreateView):
+   model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“blog:all”)
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
